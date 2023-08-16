e95d0229fe6ac0d860506e44dde0c1d85f5f5343
bywhite <bywhite@cisco.com>
2023-08-16 17:39:25 -0500

    added all policies except power

--- imm-srvt-abstract-mod/main-server-template.tf
+++ imm-srvt-abstract-mod/main-server-template.tf
@@ -45,14 +45,14 @@ resource "intersight_server_profile_template" "server_template_1" {
     moid        = var.boot_policy
     object_type = "boot.PrecisionPolicy"
   }
-#  policy_bucket {
+# policy_bucket {
 #    moid        = var.ipmi_policy == "none" ? "" : var.ipmi_policy
 #    object_type = var.ipmi_policy == "none" ? "" : "ipmioverlan.Policy"
 #  }
- policy_bucket {
+  policy_bucket {
    moid        = var.ipmi_policy
    object_type = "ipmioverlan.Policy"
- }
+  }
 policy_bucket {
     moid = var.kvm_policy
     object_type = "kvm.Policy"
@@ -61,39 +61,37 @@ policy_bucket {
 #   #   moid        = var.is_x_series_profile == true ? var.power_policy : ""
 #   #   object_type = var.is_x_series_profile == true ? "power.Policy" : ""
 #   # }
-#  policy_bucket {
-#     moid        = var.snmp_policy == "none" ? "" : var.snmp_policy
-#     object_type = var.snmp_policy == "none" ? "" : "snmp.Policy"
-#   }
-#  policy_bucket {
-#    moid        = var.sol_policy == "none" ? "" : var.sol_policy
-#    object_type = var.sol_policy == "none" ? "" : "sol.Policy"
-#  }
-#    policy_bucket {
-#    moid        = var.stor_policy == "none" ? "" : var.stor_policy
-#    object_type = var.stor_policy == "none" ? "" : "storage.StoragePolicy"
-#  }
-#   policy_bucket {
-#     moid        = var.syslog_policy == "none" ? "" : var.syslog_policy
-#     object_type = var.syslog_policy == "none" ? "" : "syslog.Policy"
-#   }
-#   policy_bucket {
-#     moid        = var.vmedia_policy == "none" ? "" : var.vmedia_policy
-#     object_type = var.vmedia_policy == "none" ? "" : "vmedia.Policy"
-#   }
-#   # az common user policy
-#   policy_bucket {
-#     moid = var.user_policy
-#     object_type = "iam.EndPointUserPolicy"
-#   }
-  # policy_bucket {
-  #   moid = var.lancon_policy
-  #   object_type = "vnic.LanConnectivityPolicy"
-  # }
-  # # san connectivity policy per server profile template
-  # policy_bucket {
-  #   moid        = var.sancon_policy
-  #   object_type = "vnic.SanConnectivityPolicy"
-  # }
+  policy_bucket {
+    moid        = var.snmp_policy
+    object_type = "snmp.Policy"
+  }
+  policy_bucket {
+   moid        = var.sol_policy
+   object_type = "sol.Policy"
+  }
+  policy_bucket {
+   moid        = var.stor_policy
+   object_type = "storage.StoragePolicy"
+  }
+  policy_bucket {
+    moid        = var.syslog_policy
+    object_type = "syslog.Policy"
+  }
+  policy_bucket {
+    moid        = var.vmedia_policy
+    object_type = "vmedia.Policy"
+  }
+  policy_bucket {
+    moid        = var.user_policy
+    object_type = "iam.EndPointUserPolicy"
+  }
+  policy_bucket {
+    moid = var.lancon_policy
+    object_type = "vnic.LanConnectivityPolicy"
+  }
+  policy_bucket {
+    moid        = var.sancon_policy
+    object_type = "vnic.SanConnectivityPolicy"
+  }
 
 }
